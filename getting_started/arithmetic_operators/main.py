c15cc1aa7cf23ab6e233d0a70c392f07bb9fe59c
mtsanghl <>
2026-01-28 10:26:14 +0000

    Arithmetic Operators

---
+++ getting_started/arithmetic_operators/main.py
@@ -0,0 +1,11 @@
+# Calculate addition
+print(5.42 + 4.39) 
+
+# Calculate subtraction
+print(12.30 - 5.50) 
+
+# Calculate multiplication
+print(5.42 * 4.39)
+
+# Calculate division
+print(5.42 / 4.39)
